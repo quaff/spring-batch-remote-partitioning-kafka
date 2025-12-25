eb8e331095e51007b04481abec4461bc0d4d9075
Yanming Zhou <zhouyanming@gmail.com>
2025-12-25 09:59:19 +0800

    Refine dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,10 +13,9 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-batch")
-	implementation("org.springframework.batch:spring-batch-integration")
 	implementation("org.springframework.boot:spring-boot-starter-integration")
+	implementation("org.springframework.batch:spring-batch-integration")
 	implementation("org.springframework.integration:spring-integration-kafka")
-	implementation("com.fasterxml.jackson.core:jackson-databind")
 	runtimeOnly("org.springframework.boot:spring-boot-docker-compose")
 	runtimeOnly("com.mysql:mysql-connector-j")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
@@ -27,6 +26,6 @@ dependencies {
 	testImplementation("org.testcontainers:kafka")
 }
 
-tasks.withType<Test>() {
+tasks.withType<Test> {
 	useJUnitPlatform()
 }
